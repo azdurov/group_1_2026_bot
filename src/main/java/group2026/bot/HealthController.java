69b981949888c3b32200065cdd96f004c4c37a19
azdurov <zdurovandrei@gmail.com>
2026-07-16 20:05:43 +0300

    keep-alive

---
+++ src/main/java/group2026/bot/HealthController.java
@@ -0,0 +1,13 @@
+package group2026.bot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
