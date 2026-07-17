0004ffcf4103e1a5549f906e8f3de1d1ae3e421c
azdurov <zdurovandrei@gmail.com>
2026-07-17 10:15:23 +0300

    Enable Scheduling

--- src/main/java/group2026/Group1BotApplication.java
+++ src/main/java/group2026/Group1BotApplication.java
@@ -2,8 +2,10 @@ package group2026;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Group1BotApplication {
     public static void main(String[] args) {
         SpringApplication.run(Group1BotApplication.class, args);
